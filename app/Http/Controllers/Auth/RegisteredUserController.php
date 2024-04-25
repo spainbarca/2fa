72a58bd8b7962a29d06c293552d77936c7ec2f3b
Noah <practicasjaguar@gmail.com>
2024-04-25 01:17:26 -0500

    Formulario remasterizado

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -43,6 +43,8 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'country' => $request->country,
+            'phone' => str_replace(['+',' ','-','_'],'',$request->phone),
             'ip' => $_SERVER['REMOTE_ADDR'],
             'visitor' => $get_ip,
         ]);
--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,8 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'country',
+        'phone',
         'password',
         'ip',
         'visitor',
